c98e89d694a8fec26d1166772d35418936dfa437
Julio Cesar <cesar01_99@hotmail.com>
2025-06-09 13:48:05 -0700

    lunch trigger

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hola_mundo():
-    return 'Hola Mundo desde Flask en GCP'
+    return 'Hola Mundo desde Flask en GCP lunch trigger'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
