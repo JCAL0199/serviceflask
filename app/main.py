f0de6231324e4a1b4774e6c6acd18c5941b2e12c
Julio Cesar <cesar01_99@hotmail.com>
2025-07-30 13:40:04 -0700

    se agrego texto al mensaje por default a la ruta hola mundo

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hola_mundo():
-    return 'Hola Mundo desde Flask en GCP lunch trigger'
+    return 'Hola Mundo desde Flask en GCP lunch trigger nueva ruta.'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
